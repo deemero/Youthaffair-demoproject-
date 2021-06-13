54dcd24908312b9111095d9fed09e29c9cfc543f
deemero <neroalex93@gmail.com>
2021-06-13 15:52:58 +0800

    make it better

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,14 @@
 const { environment } = require("@rails/webpacker");
-
-const webpack = require('webpack');
-
-environment.plugins.append(
-  'Provide',
- new webpack.ProvidePlugin({
-  $: 'jQuery'
-  // jQuery: 'jquery'
-  Popper: ['popper.js', 'defualt']
-})
-);
+// 
+// const webpack = require('webpack');
+// //
+// environment.plugins.append(
+//   'Provide',
+//  new webpack.ProvidePlugin({
+//   $: 'jQuery'
+//   // jQuery: 'jquery'
+//   Popper: ['popper.js', 'defualt']
+// })
+// );
 
 module.exports = environment;
